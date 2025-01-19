8963c51c592ea0257a47b6d6e87ed0b3d0e70f00
mmt817 <1669404371@qq.com>
2025-01-19 22:04:59 +0800

    test: - 添加e2e测试代码 - 添加vitest单元测试代码

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,7 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      '@typescript-eslint/no-explicit-any': 'off', // 禁用 any 类型警告
     },
   },
 )
